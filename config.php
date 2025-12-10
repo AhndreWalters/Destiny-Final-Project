de9157ff6603f446989579660937ce868624170a
destiny326 <destinyrma4@gmail.com>
2025-12-09 20:14:55 -1200

    upload

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials.*/
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'stagepass');
-define('DB_PASSWORD', 'KILLKINGS41!');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
 define('DB_NAME', 'stagedb');
-define('DB_PORT', '3306')
+define('DB_PORT', '3306');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
